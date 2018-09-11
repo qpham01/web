f2469cf747d9017147c79914090242bb0c4108f1
= <quoc_pham@msn.com>
2018-09-10 22:14:06 -0500

    Added question controller and view model.

--- TestMakerFree/TestMakerFreeWebApp/Controllers/QuizController.cs
+++ TestMakerFree/TestMakerFreeWebApp/Controllers/QuizController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using TestMakerFreeWebApp.ViewModels;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TestMakerFreeWebApp.Controllers
 {
@@ -41,6 +42,46 @@ namespace TestMakerFreeWebApp.Controllers
             // output the result in JSON format 
             return new JsonResult(
                 sampleQuizzes,
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
+        }
+
+        /// <summary>
+        /// GET: api/quiz/ByTitle
+        /// Retrieves the {num} Quizzes sorted by Title (A to Z)
+        /// </summary>
+        /// <param name="num">the number of quizzes to retrieve</param>
+        /// <returns>{num} Quizzes sorted by Title</returns>
+        [HttpGet("ByTitle/{num:int?}")]
+        public IActionResult ByTitle(int num = 10)
+        {
+            var sampleQuizzes = ((JsonResult)Latest(num)).Value
+                as List<QuizViewModel>;
+
+            return new JsonResult(
+                sampleQuizzes.OrderBy(t => t.Title),
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
+        }
+
+        /// <summary>
+        /// GET: api/quiz/mostViewed
+        /// Retrieves the {num} random Quizzes
+        /// </summary>
+        /// <param name="num">the number of quizzes to retrieve</param>
+        /// <returns>{num} random Quizzes</returns>
+        [HttpGet("Random/{num:int?}")]
+        public IActionResult Random(int num = 10)
+        {
+            var sampleQuizzes = ((JsonResult)Latest(num)).Value
+                as List<QuizViewModel>;
+
+            return new JsonResult(
+                sampleQuizzes.OrderBy(t => Guid.NewGuid()),
                 new JsonSerializerSettings()
                 {
                     Formatting = Formatting.Indented
